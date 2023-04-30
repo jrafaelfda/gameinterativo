a50ee8de9d22fa17af273903e7bbdbc7ed5990e5
MarcusLeão <marcuscarneironunes@gmail.com>
2023-04-30 17:24:46 -0300

    Pantano

---
+++ gameinterativo/GameInterativo/src/Pantano.java
@@ -0,0 +1,6 @@
+public class Pantano extends Local {
+    public Pantano() {
+        super("Pântano", "Um pântano é uma área encharcada, com água parada, lamaçal e vegetação densa. O ambiente é úmido e escuro, com neblina. O solo é instável e pode afundar, criando armadilhas perigosas. O pântano é lar de criaturas perigosas, como crocodilos, cobras venenosas e monstros míticos. ");
+        // Iremos adicionar personagens e escolhas disponíveis neste local
+    }
+}
